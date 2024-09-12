2a4b10ad1141be0348da25528bcce30ec99e6138
Mendoza <kenderacademico@gmail.com>
2024-09-12 19:43:06 -0400

    crear mini pagina o buscar una forma de mostrar toda la tabla productos

--- admin/productos.php
+++ admin/productos.php
@@ -136,69 +136,9 @@ $respuesta=mysqli_query($conexion,$consulta);
 while($fila=mysqli_fetch_array($respuesta))
 {
 
-echo "el id del producto buscado es: ".$fila['id_producto'];
-echo "Descripcion del producto buscado es: ".$fila['descrip_producto'];
+//echo "el id del producto buscado es: ".$fila['id_producto'];
+//echo "Descripcion del producto buscado es: ".$fila['descrip_producto'];
 ?>
-<!-- 
-<table border="2">
-
-	<tr>
-		<th>CI</th>
-		<th>NOMBRE</th>
-		<th>APELLIDO PATERNO</th>
-		<th>APELLIDO MATERNO</th>
-		<th>GENERO</th>
-		<th>EDAD</th>
-		<th>CELULAR</th>
-		<th>CONTRASEÑA</th>
-		<th>ROL</th>
-		<th>ELIMINAR</th>
-		<th>MODIFICAR</th>
-
-	</tr>
-	<tr>
-		<td><?php //echo $fila['ci'];?></td>
-		<td><?php //echo $fila['nombre'];?></td>
-			<td><?php //echo $fila['ap_pat'];?></td>
-			<td><?php //echo $fila['ap_mat'];?></td>
-			<td><?php //echo $fila['genero'];?></td>
-			<td><?php //echo $fila['edad'];?></td>
-			<td><?php //echo $fila['celular'];?></td>
-			<td><?php //echo $fila['contrasena'];?></td>
-			<td><?php //echo $fila['id_sesion'];?></td>
-			<?php //$ci=$fila['ci']; 
-			//echo "el ci buscado es: ".$ci;
-			?>
-
-			<td><form action="control/abm.php" method="post">
-				<input type="hidden" name="ci" value="<?php //echo $ci;?>">
-			<input type="submit" name="btn1" value="ELIMINAR">	
-			</form></td>
-			<td>
-				<form action="modificar.php" method="post">
-				<input type="hidden" name="ci" value=" <?php // echo $ci;?>">
-			<input type="submit" name="btn2" value="MODIFICAR">	
-			</form>
-			</td>
-	</tr>
-
-
-</table>
- -->
-
-
-
-
-<!-- 
-<p>REGISTRO DE NUEVO ESTUDIANTE</p>
-<form action="registro.php">
-	
-	<input type="submit" value="REGISTRAR NUEVO ESTUDIANTE">
-</form>
-
- -->
-
-
 		<!-- Panel listado de busqueda de administradores -->
 		<div class="container-fluid">
 			<div class="panel panel-primary">
@@ -234,6 +174,16 @@ echo "Descripcion del producto buscado es: ".$fila['descrip_producto'];
 									<?php //$ci=$fila['ci']; 
 									//echo "el ci buscado es: ".$ci;
 									?>
+									<!-- <td><form action="control/abm.php" method="post">
+										<input type="hidden" name="ci" value="<?php //echo $ci;?>">
+										<input type="submit" name="btn1" value="ELIMINAR">	
+										</form></td>
+										<td>
+											<form action="modificar.php" method="post">
+											<input type="hidden" name="ci" value=" <?php // echo $ci;?>">
+										<input type="submit" name="btn2" value="MODIFICAR">	
+										</form>
+										</td> -->
 									<td>
 										<a href="#!" class="btn btn-success btn-raised btn-xs">
 											<i class="zmdi zmdi-refresh"></i>
@@ -251,6 +201,14 @@ echo "Descripcion del producto buscado es: ".$fila['descrip_producto'];
 											</button>
 										</form>
 									</td>
+									<!-- 
+										<p>REGISTRO DE NUEVO ESTUDIANTE</p>
+										<form action="registro.php">
+
+											<input type="submit" value="REGISTRAR NUEVO ESTUDIANTE">
+										</form>
+
+										 -->
 								</tr>
 							</tbody>
 						</table>
