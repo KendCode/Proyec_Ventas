3922cd296d451d42393068a687a6bb46c228e3a4
KendCode <kenderacademico@gmail.com>
2024-09-22 12:26:22 -0400

    se modifico los datos de conexion

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -10,7 +10,6 @@ $servidor ="localhost";
 //conexion a la base de datos
 //mysql_connect(servidor, usuario, contrasena, base de datos) 
 //OR DIE MENSAJE PARA SABER SI SE CONECTO O NO 
-//$conexion = mysqli_connect("$servidor", "$usuario", "$contrasena", "$bd_datos") or die ("no se conecto a la base de datos, contactese con soporte tecnico");
 $conexion = mysqli_connect("$servidor", "$usuario", "$contrasena", "$bd_datos") or die ("no se conecto a la base de datos, contactese con soporte tecnico");
 
 ?>
